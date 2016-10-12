0ecac1901297b9033769bb7b69e9fd8e4ae125ae
Nancy De Smet <ciek.eu@gmail.com>
2016-10-12 10:03:20 +0200

    working flash app (Android 6 and higher)

--- app/src/main/java/be/ciesites/myflash/MainActivity.java
+++ app/src/main/java/be/ciesites/myflash/MainActivity.java
@@ -1,66 +1,85 @@
 package be.ciesites.myflash;
 
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.graphics.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean hasFlash;
-
     private CameraManager manager;
-
     private ToggleButton toggleButton;
+    private String flashCameraId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
 
-        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
-
-       // hasFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-
-
-        manager = (CameraManager) getSystemService(CAMERA_SERVICE);
-
-
-
-
-
-
-
-        manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+       toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+       manager = (CameraManager) getSystemService(CAMERA_SERVICE);
 
         try {
             String[] cameraIdList = manager.getCameraIdList();
             for (String cameraId : cameraIdList){
                 hasFlash = manager.getCameraCharacteristics(cameraId).get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
-                if (!hasFlash) {
-
-                    AlertDialog alert = new AlertDialog.Builder(this).create();
-                    alert.setTitle("Error");
-                    alert.setMessage("Your device does not support a flash");
-                    alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                        }
-                    });
-                }   else {
-                    //TODO select
+                if (hasFlash) {
+                    flashCameraId = cameraId;
+                    break;
                 }
-
             }
         } catch (CameraAccessException e) {
-            e.printStackTrace();
+           showAlert();
         }
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setFlash(isChecked);
+            }
+        });
+    }
+
+    private void setFlash(boolean isChecked) {
+        Log.d("MyFlash", "Setting Torch Mode to " + isChecked);
+        if (manager == null || flashCameraId == null) return;
+        try {
+            manager.setTorchMode(flashCameraId, isChecked);
+        } catch (CameraAccessException e) {
+            Log.e("MyFlash", "Unable to change the Torch Mode", e);
+        }
+        toggleButton.setChecked(isChecked);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+       setFlash(false);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setFlash(false);
+    }
+
+    private void showAlert() {
+        AlertDialog alert = new AlertDialog.Builder(this).create();
+        alert.setTitle("Error");
+        alert.setMessage("Your device does not support a flash");
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alert.show();
     }
 }
